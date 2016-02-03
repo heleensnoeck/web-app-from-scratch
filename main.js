175cde4ce75f6ba1c322bf431a9d7122b76e0527
heleensnoeck <heleensnoeck@msn.com>
2016-02-03 12:15:38 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-
 (function() {
  
-        'use Strict';
+        'Use Strict'; // je mag niet zonder var werken bijv. zodat je netjes blijft werken je moet er een var voor zetten Je krijgt een error als je bijv name = jan gebruikt
         
        var sandbox = "sandbox";
        var linear = "linear";
@@ -21,14 +20,14 @@
        var locatieRij, markerRij = [];
 
         function EventTarget() {
-            this._listeners = {}  // een leeg object
+            this._listeners = {};  // een leeg object
         }
 
         EventTarget.prototype = {
             constructor: EventTarget,
             addListener: function(a, c) {
                 "undefined" == typeof this._listeners[a] && (this._listeners[a] = []);
-                this._listeners[a].push(c)
+                this._listeners[a].push(c);
             },
             fire: function(a) {
                 "string" == typeof a && (a = {
@@ -37,7 +36,7 @@
                 a.target || (a.target = this);
                 if (!a.type) throw Error("Event object missing 'type' property.");
                 if (this._listeners[a.type] instanceof Array)
-                    for (var c = this._listeners[a.type], b = 0, d = c.length; b < d; b++) c[b].call(this, a)
+                    for (var c = this._listeners[a.type], b = 0, d = c.length; b < d; b++) c[b].call(this, a);
             },
             removeListener: function(a, c) {
                 if (this._listeners[a] instanceof Array)
@@ -45,7 +44,7 @@
                             this._listeners[a], d = 0, e = b.length; d < e; d++)
                         if (b[d] === c) {
                             b.splice(d, 1);
-                            break
+                            break;
                         }
             }
         };
@@ -54,22 +53,22 @@
 
         var geo = {
             // Test of GPS beschikbaar is (via geo.js) en vuur een event af
-            function init() {
+            init: function() {
                 debugMessage("Controleer of GPS beschikbaar is...");
 
-                et.addListener(gpsAvailable, startInterval);
-                et.addListener(gpsAvailable, function() {
+                et.addListener(this.gpsAvailable, startInterval);
+                et.addListener(this.gpsAvailable, function() {
                     debugMessage('GPS is niet beschikbaar.')
                 });
 
-                (geoPositionJs.init()) ? et.fire(gpsAvailable): et.fire(gpsUnavailable); // hier roept hij ze aan 
+                (geoPositionJs.init()) ? et.fire(this.gpsAvailable): et.fire(this.gpsUnavailable); // hier roept hij ze aan 
             }
 
             // Start een interval welke op basis van refrashRate de positie updated
-            function startInterval(event) {
+            startInterval: function(event) {
                 debugMessage("GPS is beschikbaar, vraag positie.");
-                updatePosition(); // roept update position aan
-                interval = self.setInterval(updatePosition, refreshRate); /////////////////////// net als settimeout setinterval kijk naar variable refreshrate
+                this.updatePosition(); // roept update position aan
+                interval = self.setInterval(this.updatePosition, this.refreshRate); /////////////////////// net als settimeout setinterval kijk naar variable refreshrate
 
                 et.addListener(positionUpdated, checkLocations);
             }
@@ -77,22 +76,22 @@
             //////////////////////////////////////////////////////////////////// DE FUNCTIE HIERBOVEN MOET in function init
 
             // Vraag de huidige positie aan geo.js, stel een callback in voor het resultaat
-            function updatePosition() {
+            updatePosition: function() {
                 intervalCounter++;
-                geoPositionJs.get.CurrentPosition(setPosition, geoErrorHandler, {
+                geoPositionJs.get.this.CurrentPosition(setPosition, geoErrorHandler, {
                     enableHighAccuracy: true
                 });
             }
 
             // Callback functie voor het instellen van de huidige positie, vuurt een event af
-            function setPosition(position) {
-                currentPosition = position;
+            setPosition: function(position) {
+                this.currentPosition = position;
                 et.fire("positionUpdated");
                 debugMessage(intervalCounter + " positie lat:" + position.coords.latitude + " long:" + position.coords.longitude);
             }
 
             // Controleer de locaties en verwijs naar een andere pagina als we op een locatie zijn
-            function checkLocations(event) {
+            checkLocations: function(event) {
                 // Liefst buiten google maps om... maar helaas, ze hebben alle coole functies
                 for (var i = 0; i < locaties.length; i++) {
                     var locatie = {
@@ -102,7 +101,7 @@
                         }
                     };
 
-                    if (_calculate_distance(locatie, currentPosition) < locaties[i][2]) {
+                    if (_calculate_distance(locatie, this.currentPosition) < locaties[i][2]) {
 
                         // Controle of we NU op die locatie zijn, zo niet gaan we naar de betreffende page
                         if (window.location != locaties[i][1] && localStorage[locaties[i][0]] == "false") {
@@ -123,27 +122,14 @@
             }
 
             // Bereken het verchil in meters tussen twee punten
-            function _calculate_distance(p1, p2) {
+            _calculate_distance: function(p1, p2) {
                 var pos1 = new google.maps.LatLng(p1.coords.latitude, p1.coords.longitude);
                 var pos2 = new google.maps.LatLng(p2.coords.latitude, p2.coords.longitude);
                 return Math.round(google.maps.geometry.spherical.computeDistanceBetween(pos1, pos2), 0);
             }
 
 
-            // GOOGLE MAPS FUNCTIES
-            /**
-             * generate_map(myOptions, canvasId)
-             *  roept op basis van meegegeven opties de google maps API aan
-             *  om een kaart te genereren en plaatst deze in het HTML element
-             *  wat aangeduid wordt door het meegegeven id.
-             *
-             *  @param myOptions:object - een object met in te stellen opties
-             *      voor de aanroep van de google maps API, kijk voor een over-
-             *      zicht van mogelijke opties op http://
-             *  @param canvasID:string - het id van het HTML element waar de
-             *      kaart in ge-rendered moet worden, <div> of <canvas>
-             */
-            function generate_map(myOptions, canvasId) {
+            generate_map: function(myOptions, canvasId) {
                 // TODO: Kan ik hier asynchroon nog de google maps api aanroepen? dit scheelt calls
                 debugMessage("Genereer een Google Maps kaart en toon deze in #" + canvasId)
                 map = new google.maps.Map(document.getElementById(canvasId), myOptions);
@@ -192,7 +178,7 @@
                 }
 
                 // Voeg de locatie van de persoon door
-                currentPositionMarker = new google.maps.Marker({
+                this.currentPositionMarker = new google.maps.Marker({
                     position: kaartOpties.center,
                     map: map,
                     icon: positieMarker,
@@ -202,31 +188,35 @@
                 // Zorg dat de kaart geupdated wordt als het POSITION_UPDATED event afgevuurd wordt
                 ET.addListener(positionUpdated, update_positie);
             }
+// utilitie object
 
-            function isNumber(n) {
+		var utilities = {
+            
+            isNumber: function (n) {
                 return !isNaN(parseFloat(n)) && isFinite(n);
             }
 
             // Update de positie van de gebruiker op de kaart
-            function updatePosition(event) {
-                // use currentPosition to center the map
-                var newPos = new google.maps.LatLng(currentPosition.coords.latitude, currentPosition.coords.longitude);
-                map.setCenter(newPos);
-                currentPositionMarker.setPosition(newPos);
-            }
-        }
+                updatePosition function(event)  {
+                    // use currentPosition to center the map
+                    var newPos = new google.maps.LatLng(this.currentPosition.coords.latitude, this.currentPosition.coords.longitude);
+                    map.setCenter(newPos);
+                    this.currentPositionMarker.setPosition(newPos);
+                }
+        	}
+        };
 
         var debug = {
         // FUNCTIES VOOR DEBUGGING
-            function geoErrorHandler(code, message) {
-                debugMessage('geo.js error ' + code + ': ' + message);
-            }
+          geoErrorHandler: function: (code, message) {
+                this.debugMessage('geo.js error ' + code + ': ' + message);
+            },
 
-            function debugMessage(message) {
-                (customDebugging && debugId) ? document.getElementById(debugId).innerHTML: console.log(message);
-            }
+            debugMessage: function (message) {
+                (this.customDebugging && debugId) ? document.getElementById(debugId).innerHTML: console.log(message);
+            },
 
-            function setCustomDebugging(debugId) {
+             setCustomDebugging: function (debugId) {
                 debugId = debugId;
                 customDebugging = true;
             }
